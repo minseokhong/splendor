6b590b19d5ba108d3b1ea0989949bdc32c90d8c6
YunHyunBeen <nsntks19@naver.com>
2019-05-02 12:51:48 +0900

    ㅁㅁ

--- splendor/src/main/java/com/rng/splendor/db/controller/JspController.java
+++ splendor/src/main/java/com/rng/splendor/db/controller/JspController.java
@@ -7,13 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.rng.splendor.db.dto.Test;
 import com.rng.splendor.db.dto.UserData;
 import com.rng.splendor.db.service.TestService;
 import com.rng.splendor.db.service.UserService;
